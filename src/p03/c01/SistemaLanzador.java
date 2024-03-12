4b625890dc081fdb70416f93ec0fbd2740b561d7
Rafael Martin <rmg1008@alu.ubu.es>
2024-03-12 19:39:53 +0100

    Se modifica la clase SistemaLanzador

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -1,26 +1,34 @@
 package src.p03.c01;
 
 public class SistemaLanzador {
-	public static void main(String[] args) {
+	private final static int NUM_PUERTAS = 5; // Valor por defecto
+	
+	public static void main(String[] args) 
+	{
+		int nPuertas = NUM_PUERTAS; 
 		
-		IParque parque = new Parque(); // TODO
+		// Se toma el valor si se pasa por parámetro
+		if (args != null && args.length > 0)
+		{
+			nPuertas = Integer.parseInt(args[0]);
+		}
+		IParque parque = new Parque();  // Instanciamos clase Parque
 		char letra_puerta = 'A';
 		
 		System.out.println("¡Parque abierto!");
 		
-		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
-			
-			String puerta = ""+((char) (letra_puerta++));
+		for (int i = 0; i < nPuertas; i++) 
+		{	
+			String puerta = ""+(letra_puerta++);
 			
 			// Creación de hilos de entrada
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
 			new Thread (entradas).start();
 			
-			// 
-			// TODO
-			//
-			
+			// Creación de hilos de salida
+			ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
+			new Thread (salidas).start();
 			
 		}
-	}	
+	}
 }
