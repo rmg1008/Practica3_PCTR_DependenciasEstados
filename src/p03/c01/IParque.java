2608938f76d6ecc97805c62b8d683585d9fa61a3
Rafael Martin <rmg1008@alu.ubu.es>
2024-03-12 19:37:59 +0100

    Se añade método al interfaz IParque

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -2,8 +2,13 @@ package src.p03.c01;
 
 public interface IParque {
 	
+	/*
+	 * Método para la entrada al parque
+	 */
 	public abstract void entrarAlParque(String puerta);
 
-	// TODO
-
+	/*
+	 * Método para la salida del parque
+	 */
+	public abstract void salirDelParque(String puerta);
 }
